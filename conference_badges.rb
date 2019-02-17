5cd63b91f56926c49ea56c6309532ae82f4e78b2
John Wan <elite_floral@msn.com>
2019-02-17 20:39:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ badge_maker[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 # Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
 batch_badge_creator
 def add_prefix(badge_maker)
-return "Hello, My name is + name"
+return "Hello, My name is + badge_maker"
 end
 # assign_rooms
 # Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
@@ -14,6 +14,7 @@ assign_rooms[100, 101, 102, 103, 104, 105, 106]
 # return a list of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
 def add_prefix(assign_rooms)
   return "Hello[badge_maker]!  You'll be assigned to room [assign_rooms]"
+end
   # printer
   # Create a method called printer that will output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen.
   # Hint: Remember that methods can call other methods. If the return value of assign_rooms is an array of room assignments, how can you print out each assignment? You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
