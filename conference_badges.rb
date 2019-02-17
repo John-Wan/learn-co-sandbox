0aab172dd73664a878bbe01e12d06d7a44915d28
John Wan <elite_floral@msn.com>
2019-02-17 20:55:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 # assign_rooms
 # Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
 # assign_rooms
-assign_rooms = [100, 101, 102, 103, 104, 105, 106]
+assign_rooms = "100", "101", "102", "103", "104", "105", "106"
 # return a list of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
 def add_prefix(assign_rooms)
   return "Hello[badge_maker]!  You'll be assigned to room [assign_rooms]"
