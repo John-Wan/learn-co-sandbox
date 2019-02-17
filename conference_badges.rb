72a77e595462c1272ca522532e73dfff3ab70b19
John Wan <elite_floral@msn.com>
2019-02-17 20:15:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,5 @@ badge_maker("Arel")
 # Each badge should read: "Hello, my name is _____."
 name[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 # Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
+def add_prefix(name)
+return "Hello, My name is + name"
