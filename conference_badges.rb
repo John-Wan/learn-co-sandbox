83805a56f83a7e6f8cfce136e0a11e798b783688
John Wan <elite_floral@msn.com>
2019-02-17 20:32:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,7 @@ def add_prefix(assign_rooms)
   return "Hello[badge_maker]!  You'll be assigned to room [assign_rooms]"
   # printer
   # Create a method called printer that will output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen.
-  # 
+  # Hint: Remember that methods can call other methods. If the return value of assign_rooms is an array of room assignments, how can you print out each assignment? You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
+  # ???
+  
 
