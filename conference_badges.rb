9627f4ed3584728cd085cea2a9c5735fbaa009ce
John Wan <elite_floral@msn.com>
2019-02-17 20:29:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,18 @@
 badge_maker("Arel")
 # Hello, "My name is Arel."
 # Each badge should read: "Hello, my name is _____."
-name[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+badge_maker[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 # Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
-def add_prefix(name)
+batch_badge_creator
+def add_prefix(badge_maker)
 return "Hello, My name is + name"
 end
 # assign_rooms
 # Hint: Remember that the return value of the each method is the original array that you are calling it on. How can you collect or store the room assignment strings you are creating as you iterate and return them at the end of your assign_rooms method? Google or use Ruby Docs to find the correct method.
+assign_rooms
 assign_rooms[100, 101, 102, 103, 104, 105, 106]
-#
+# return a list of room assignments in the form of: "Hello, _____! You'll be assigned to room _____!"
+def add_prefix(assign_rooms)
+  return "Hello[badge_maker]!  You'll be assigned to room [assign_rooms]"
+  
+
