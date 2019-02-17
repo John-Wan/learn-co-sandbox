15694ff3315052c479ee573195ef2ac8820eca13
John Wan <elite_floral@msn.com>
2019-02-17 20:17:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,4 @@ name[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 # Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
 def add_prefix(name)
 return "Hello, My name is + name"
+end
